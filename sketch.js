9aeba18c3a5d972e8d1ca0999c2cf962e101870d
ANDREW-ALUNO <andrew.silva@alu.apac.org.br>
2022-11-16 17:41:38 -0300

    Jogo do coelho final

--- sketch.js
+++ sketch.js
@@ -6,16 +6,22 @@ const Constraint = Matter.Constraint;
 const Body = Matter.Body;
 const Composites = Matter.Composites;
 const Composite = Matter.Composite;
-
+var ground,rope,fruit,link
 let engine;
 let world;
 
 function setup() 
 {
-  createCanvas(500,700);
+  createCanvas(500,windowHeight - 10);
   engine = Engine.create();
   world = engine.world;
  
+  ground = new Ground(250,height - 20,500,20)
+  rope = new Rope(6,{x:245,y:30})
+  fruit = Bodies.circle(300,300,15)
+  Composite.add(rope.body,fruit)
+  link = new Link(rope,fruit)
+
   rectMode(CENTER);
   ellipseMode(RADIUS);
   imageMode(CENTER);
@@ -26,7 +32,9 @@ function draw()
 {
   background(51);
   Engine.update(engine);
-   
+  ground.display()
+  rope.show()
+  ellipse(fruit.position.x,fruit.position.y,15,15)
 }
 
 
